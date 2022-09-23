5a82455f02f62751bf8b6c141f62224c35c7a221
Дмитрий Тищенко GH <107504096+DmitrTishchenko@users.noreply.github.com>
2022-09-23 17:25:05 +0300

    test1
    test1 не грузит на прямую только через сайт

--- user_interface.py
+++ user_interface.py
@@ -1,1 +1,9 @@
 
+import check as ch
+
+
+operants_1 = ch.get_number('Введите первое число \n')
+operants_2 = ch.get_number('Введите второе число \n')
+
+operantions = ch.get_number_operation(
+    'Какую операцию вы хотите выполнить? \n 1 Сложение \n 2 Вычитание \n 3 Деление \n 4 Умножение \n')
